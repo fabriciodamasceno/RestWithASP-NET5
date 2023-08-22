4d720f51cd467ba44d1b9d319a6c80e09185e054
Fabrício M. Damasceno <fabricio.damasceno@outlook.com.br>
2023-08-22 17:40:41 -0300

    Alterando estrutura de pastas

--- RestWithAspNetCore/RestWithAspNetCore/Startup.cs
+++ RestWithAspNetCore/RestWithAspNetCore/Startup.cs
@@ -44,7 +44,7 @@ namespace RestWithAspNetCore
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RestWithAspNetCore v1"));
             }
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
